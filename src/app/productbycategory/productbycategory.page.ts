0c55aab847d5e921fc3daef02dc38223280362b7
kiran <kirankumar.g@claritaz.com>
2019-08-28 17:11:19 +0530

    iran

--- src/app/productbycategory/productbycategory.page.ts
+++ src/app/productbycategory/productbycategory.page.ts
@@ -65,45 +65,7 @@ export class ProductbycategoryPage implements OnInit {
     }
     this.getproductbysubcategory(this.subcategory_id);
   }
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Albums',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          console.log('Delete clicked');
-        }
-      }, {
-        text: 'Share',
-        icon: 'share',
-        handler: () => {
-          console.log('Share clicked');
-        }
-      }, {
-        text: 'Play (open modal)',
-        icon: 'arrow-dropright-circle',
-        handler: () => {
-          console.log('Play clicked');
-        }
-      }, {
-        text: 'Favorite',
-        icon: 'heart',
-        handler: () => {
-          console.log('Favorite clicked');
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }]
-    });
-    await actionSheet.present();
-  }
+  
   // getsubcategories(category_id){
   //   this.productservice.presentLoading();
   //   this.productservice.getsubcategory(category_id)
